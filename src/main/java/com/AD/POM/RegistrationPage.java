baa398173ea09e8da296afbebee219140cf39dce
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2024-12-30 16:47:54 +0200

    adding registration page first 2 components locators

--- src/main/java/com/AD/POM/RegistrationPage.java
+++ src/main/java/com/AD/POM/RegistrationPage.java
@@ -2,12 +2,19 @@ package com.AD.POM;
 
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationPage  extends BasePage {
     //1. CONST
     public static final String REGISTARATION_PAGE_URL = "/users/register";
     //2.UI MAP
+    @FindBy(css = "h4.text-center.mb-4")
+    private WebElement regFormTitle;
+
+    @FindBy(css = "input.is-invalid")
+    private WebElement usernameInputField;
 
     //3 CONSTRUCTOR
     public RegistrationPage(WebDriver driver, Logger log) {
