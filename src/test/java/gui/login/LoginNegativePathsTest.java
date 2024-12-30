102e0d94a2c1e042ea85a285a84243ee13d38677
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2024-12-30 16:06:13 +0200

    makeing negative paths with DataProvider

--- src/test/java/gui/login/LoginNegativePathsTest.java
+++ src/test/java/gui/login/LoginNegativePathsTest.java
@@ -5,6 +5,8 @@ import com.AD.POM.LoginPage;
 import gui.base.BaseTest;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginNegativePathsTest extends BaseTest {
@@ -12,14 +14,25 @@ public class LoginNegativePathsTest extends BaseTest {
     private static final String LOGIN_FORM_TITLE = "Sign in";
     public static final String LOGIN_NOT_SUCCESSFUL_MSG = "Wrong username or password!";
 
-    @Test
-    public void verifyUserCannotLoginWithWrongUserName() {
+    @DataProvider(name = "invalidCredentials")
+    public Object[][] invalidCredentials() {
+        return new Object[][]{
+                {"HarryPotter", "Dumbeldor123"},
+                {" ", "Password123"},
+                {"Nasko10", "Password9090"},
+                {"Nasko10", " "},
+                {" ", " "}
+        };
+    }
+
+    @Test(dataProvider = "invalidCredentials")
+    public void verifyUserCannotLoginWithWrongUserName(String username, String password) {
         LoginPage loginPage = new LoginPage(super.driver, log);
 
-        log.info("STEP 1: Already registered user is landing on Iskilo login page.");
+        log.info("STEP 1: Landing on Iskilo login page.");
         loginPage.navigateToLoginPage();
 
-        log.info("STEP 2: Verify the user is on login page.");
+        log.info("STEP 2: Verify login page is loaded.");
         boolean isLoginPageLoaded = loginPage.isURLLoaded(loginPage.LOGIN_PAGE);
         Assert.assertTrue(isLoginPageLoaded, "User is not on the login page.");
 
@@ -28,10 +41,10 @@ public class LoginNegativePathsTest extends BaseTest {
         Assert.assertEquals(actualLoginFormTitle, LOGIN_FORM_TITLE, "Login form title mismatch!");
 
         log.info("STEP 4: Provide invalid username.");
-        loginPage.provideUserName("HarryPotter");
+        loginPage.provideUserName(username);
 
         log.info("STEP 5: Provide invalid password.");
-        loginPage.providePassword("Dumbeldor123");
+        loginPage.providePassword(password);
 
         log.info("STEP 6: Interact with 'Remember Me' checkbox.");
         Assert.assertFalse(loginPage.isRememberMeCheckboxSelected(), "Checkbox is selected by default!");
@@ -49,51 +62,4 @@ public class LoginNegativePathsTest extends BaseTest {
         WebElement loginFormSubmitButton = loginPage.getLoginFormSubmitButton();
         Assert.assertTrue(loginFormSubmitButton.isDisplayed(), "Login form submit button is not visible!");
     }
-
-    @Test
-    public void verifyUserCannotLoginWithEmptyUserName(){
-        LoginPage loginPage = new LoginPage(super.driver,log);
-
-        log.info("STEP 1: Already registered user is landing Iskilo loinpage");
-        loginPage.navigateToLoginPage();
-        //Verification that the user is on login page
-        //Verification login form is presented
-
-        loginPage.provideUserName(" ");
-
-        loginPage.providePassword("testing");
-
-
-        loginPage.clickOnLoginButton();
-        //Assertion
-        //  Login action message shows = ERROR text
-        //If user cannot login - login button states the some =  Form visible
-        // Login button from login Form visible
-        // page ULR = some
-    }
-
-    @Test
-    public void verifyUserCannotLoginWithWrongPassword(){
-        LoginPage loginPage = new LoginPage(super.driver,log);
-        loginPage.navigateToLoginPage();
-        loginPage.provideUserName("testingDemo");
-        loginPage.providePassword("WrongPassword");
-
-    }
-
-    @Test
-    public void verifyUserCannotLoginWithEmptyPassword(){
-        LoginPage loginPage = new LoginPage(super.driver,log);
-        loginPage.navigateToLoginPage();
-        loginPage.provideUserName("testingDemo");
-        loginPage.providePassword(" ");
-
-    }
-
-    @Test
-    public void verifyUserCannotLoginWithEmptyCredentialsData(){
-        LoginPage loginPage = new LoginPage(super.driver,log);
-        loginPage.navigateToLoginPage();
-    }
-
 }
