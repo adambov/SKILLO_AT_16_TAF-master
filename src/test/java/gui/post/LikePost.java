29ec9b820bdd278f13b3f621519ae528b0ba95cf
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2025-01-06 00:19:21 +0200

    saving

--- src/test/java/gui/post/LikePost.java
+++ src/test/java/gui/post/LikePost.java
@@ -9,8 +9,8 @@ import org.testng.annotations.Test;
 import java.io.File;
 
 public class LikePost extends BaseTest {
-    public static final String testUser = "testingDemos";
-    public static final String testPassword = "testing";
+    public static final String testUser = "Nasko10";
+    public static final String testPassword = "Password123";
     public static final String caption = "Testing the create post caption";
     File postPicture = new File("src/test/resources/upload/n3Test.jpg");
 
@@ -28,16 +28,6 @@ public class LikePost extends BaseTest {
         log.info("The user has logged in with username and password.");
         loginPage.loginWithUSerAndPassword(testUser, testPassword);
 
-        log.info("The user has navigated to the Profile page.");
-        homePage.openHomePage();
-
-        log.info("The user clicked on Post button");
-        profilePage.clickPost(0);
-        log.info("The user has clicked on the first post.");
-
-        homePage.ClickOnLikeButton();
-        log.info("The user has clicked on the like button.");
-        homePage.isLikeMessageVisible();
 
     }
 }
