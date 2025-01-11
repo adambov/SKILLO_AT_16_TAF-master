7a4755bad00ec77df29b430f6fad97e8f3390dc9
Atanas Dambov <92813305+adambov@users.noreply.github.com>
2025-01-11 18:49:05 +0200

    changing REgistration page negative path with data provider

--- src/test/java/gui/registration/RegistrationNegativePath.java
+++ src/test/java/gui/registration/RegistrationNegativePath.java
@@ -3,145 +3,67 @@ package gui.registration;
 import com.AD.POM.RegistrationPage;
 import gui.base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.regData.RegistrationDataGenerator;
+
+import static utils.regData.RegistrationDataGenerator.*;
 
 
 public class RegistrationNegativePath extends BaseTest {
 
 
     String USERNAME = RegistrationDataGenerator.createUser();
-    String EMAIL = RegistrationDataGenerator.createEmail();
-    String DATE = RegistrationDataGenerator.createDate();
+    String EMAIL = createEmail();
+    String DATE = createDate();
     String PASSWORD = RegistrationDataGenerator.createPassword(6);
     String PUBLIC_INFO = RegistrationDataGenerator.generateRandomPublicInfo(50);
 
-    String INVALID_USERNAME = RegistrationDataGenerator.createInvalidUsername();
+    String INVALID_USERNAME = createInvalidUsername();
     String INVALID_EMAIL = RegistrationDataGenerator.createInvalidEmail();
     String INVALID_DATE = RegistrationDataGenerator.createInvalidDate();
     String INVALID_PASSWORD = RegistrationDataGenerator.createInvalidPassword(3);
 
-    @Test
-    public void testInvalidUsername() throws InterruptedException { //да си оправя името на функцията
-        RegistrationPage registrationPage = new RegistrationPage(super.driver,log);
-        log.info("Registration page is loaded");
-        registrationPage.navigateToRegPage();
-        log.info("Verify registration form title");
-        registrationPage.getRegFormTitle();
-        log.info("Verify if sign in button is enabled");
-        registrationPage.signUnButtonState();
-        log.info("Provide invalid username");
-        registrationPage.provideUserName(INVALID_USERNAME);
-        log.info("Provide valid mail");
-        registrationPage.provideEmail(EMAIL);
-        log.info("Provide valid date");
-        registrationPage.provideBirthDate(DATE);
-        log.info("Provide Valid password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Provide valid confirm password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Provide valid public info");
-        registrationPage.providePublicInfoText(PUBLIC_INFO);
-        log.info("Verify if Sing in button is disabled");
-        Assert.assertFalse(registrationPage.signUnButtonState(), "Button is enabled and it has to be disabled");
+    @DataProvider(name = "invalidDataProvidedForEachFieldSeparately")
+    public Object[][] provideInvalidData() {
+        return new Object[][] {
+                {INVALID_USERNAME, EMAIL, DATE, PASSWORD, PASSWORD, PUBLIC_INFO},
+                {USERNAME, INVALID_EMAIL, DATE, PASSWORD, PASSWORD, PUBLIC_INFO},
+                {USERNAME, EMAIL, INVALID_DATE, PASSWORD, PASSWORD, PUBLIC_INFO},
+                {USERNAME, EMAIL, DATE, INVALID_PASSWORD, INVALID_PASSWORD, PUBLIC_INFO},
+                {USERNAME, EMAIL, DATE, PASSWORD, PASSWORD, ""}
+        };
     }
-
-    @Test
-    public void testInvalidEmail() throws InterruptedException {
-        RegistrationPage registrationPage = new RegistrationPage(super.driver,log);
+    @Test(dataProvider = "invalidDataProvidedForEachFieldSeparately")
+    public void validateUserCannotRegisterWithInvalidData(String username, String email, String date, String password, String confirmPassword, String publicInfo) throws InterruptedException {
+        RegistrationPage registrationPage = new RegistrationPage(super.driver, log);
         log.info("Registration page is loaded");
         registrationPage.navigateToRegPage();
         log.info("Verify registration form title");
         registrationPage.getRegFormTitle();
-        log.info("Verify if sign in button is enabled");
+        log.info("Verify if sign in button is disabled");
         registrationPage.signUnButtonState();
-        log.info("Provide valid username");
-        registrationPage.provideUserName(USERNAME);
-        log.info("Provide invalid mail");
-        registrationPage.provideEmail(INVALID_EMAIL);
-        log.info("Provide valid date");
-        registrationPage.provideBirthDate(DATE);
-        log.info("Provide Valid password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Provide valid confirm password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Provide valid public info");
-        registrationPage.providePublicInfoText(PUBLIC_INFO);
-        log.info("Verify if Sing in button is disabled");
         Assert.assertFalse(registrationPage.signUnButtonState(), "Button is enabled and it has to be disabled");
-    }
 
-    @Test
-    public void testInvalidDate() throws InterruptedException {
-        RegistrationPage registrationPage = new RegistrationPage(super.driver,log);
-        log.info("Registration page is loaded");
-        registrationPage.navigateToRegPage();
-        log.info("Verify registration form title");
-        registrationPage.getRegFormTitle();
-        log.info("Verify if sign in button is enabled");
-        registrationPage.signUnButtonState();
-        log.info("Provide valid username");
-        registrationPage.provideUserName(USERNAME);
-        log.info("Provide valid mail");
-        registrationPage.provideEmail(EMAIL);
-        log.info("Provide invalid date");
-        registrationPage.provideBirthDate(INVALID_DATE);
-        log.info("Provide Valid password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Provide valid confirm password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Provide valid public info");
-        registrationPage.providePublicInfoText(PUBLIC_INFO);
-        log.info("Verify if Sing in button is disabled");
-        Assert.assertFalse(registrationPage.signUnButtonState(), "Button is enabled and it has to be disabled");
-    }
+        log.info("Provide username");
+        registrationPage.provideUserName(username);
 
-    @Test
-    public void testInvalidPassword() throws InterruptedException {
-        RegistrationPage registrationPage = new RegistrationPage(super.driver,log);
-        log.info("Registration page is loaded");
-        registrationPage.navigateToRegPage();
-        log.info("Verify registration form title");
-        registrationPage.getRegFormTitle();
-        log.info("Verify if sign in button is enabled");
-        registrationPage.signUnButtonState();
-        log.info("Provide validusername");
-        registrationPage.provideUserName(USERNAME);
-        log.info("Provide valid mail");
-        registrationPage.provideEmail(EMAIL);
-        log.info("Provide valid date");
-        registrationPage.provideBirthDate(DATE);
-        log.info("Provide invalid password");
-        registrationPage.providePassword(INVALID_PASSWORD);
-        log.info("Provide invalid confirm password");
-        registrationPage.providePassword(INVALID_PASSWORD);
-        log.info("Provide valid public info");
-        registrationPage.providePublicInfoText(PUBLIC_INFO);
-        log.info("Verify if Sing in button is disabled");
-        Assert.assertFalse(registrationPage.signUnButtonState(), "Button is enabled and it has to be disabled");
-    }
+        log.info("Provide email");
+        registrationPage.provideEmail(email);
 
-    @Test
-    public void testEmptyPublicInfo() throws InterruptedException {
-        RegistrationPage registrationPage = new RegistrationPage(super.driver,log);
-        log.info("Registration page is loaded");
-        registrationPage.navigateToRegPage();
-        log.info("Verify registration form title");
-        registrationPage.getRegFormTitle();
-        log.info("Verify if sign in button is enabled");
-        registrationPage.signUnButtonState();
-        log.info("Provide validusername");
-        registrationPage.provideUserName(USERNAME);
-        log.info("Provide valid mail");
-        registrationPage.provideEmail(EMAIL);
-        log.info("Provide valid date");
-        registrationPage.provideBirthDate(DATE);
-        log.info("Provide Valid password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Provide valid confirm password");
-        registrationPage.providePassword(PASSWORD);
-        log.info("Public info is left empty");
-        log.info("Verify if Sing in button is disabled");
+        log.info("Provide birth date");
+        registrationPage.provideBirthDate(date);
+
+        log.info("Provide password");
+        registrationPage.providePassword(password);
+
+        log.info("Provide confirm password");
+        registrationPage.providePassword(confirmPassword);
+
+        log.info("Provide public info");
+        registrationPage.providePublicInfoText(publicInfo);
+
+        log.info("Verify if sign in button is disabled");
         Assert.assertFalse(registrationPage.signUnButtonState(), "Button is enabled and it has to be disabled");
     }
 
